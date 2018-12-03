45b15f2d76ccab846bfa96eb23305f2b09fde508
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-03 15:51:02 -0700

    Additional store functional tests (page content)

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -5,8 +5,11 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     get store_index_url
     assert_response :success
     assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'nav.side_nav a', 'Home'
+    assert_select 'nav.side_nav a', 'News'
     assert_select 'main ul.catalog li', 3
     assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select 'h2 ~ p', /fast growing language/
     assert_select '.price', /\$[\d,]+\.\d{,2}/
   end
 end
