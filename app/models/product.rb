cb7e3f95243eaeb9cd7894cc29b254d790156945
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-03 11:17:03 -0700

    Add length validations and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 8, maximum: 100 }
+  validates :description, length: { minimum: 20, maximum: 1000 }
   validates :price, numericality: { 
     greater_than_or_equal_to: 0.01,
     message: 'must be greater than 0.00'
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -12,26 +12,24 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
   end
 
-  def new_product(price_var)
-    Product.new(title: "Title",
-                description: "An item",
+  def new_product(price_val, title_val="This is the default title.")
+    Product.new(title: title_val,
+                description: "An item description here!",
                 image_url: "somefile.jpg",
-                price: price_var)
+                price: price_val)
   end
 
   test "product price must not be negative" do
     negative_prices = [-1.00, -0.01, -100.00, -20.00]
 
-    negative_prices.each do |price_var|
-      assert new_product(price_var).invalid?
+    negative_prices.each do |price|
+      assert new_product(price).invalid?
     end
   end
 
   test "product price must not be zero" do
-    product = Product.new(title: "Title",
-                          description: "An item",
-                          image_url: "somefile.jpg",
-                          price: 0.00)
+    product = new_product(0.00)
+
     assert product.invalid?
     assert_equal ["must be greater than 0.00"],
                   product.errors[:price]
@@ -40,35 +38,65 @@ class ProductTest < ActiveSupport::TestCase
   test "product price accepted equal or greater than 0.01" do
     valid_prices = [0.01, 0.02, 100.00, 12, 192.01]
 
-    valid_prices.each do |price_var|
-        assert new_product(price_var).valid?
+    valid_prices.each do |price|
+        assert new_product(price).valid?
     end
   end
 
   test "product must not have non-numeric value" do
     non_numeric_prices = %w{one two nineteen zero forty eleven}
     
-    non_numeric_prices.each do |price_var|
-      assert new_product(price_var).invalid?
+    non_numeric_prices.each do |price|
+      assert new_product(price).invalid?
     end
   end
 
   test "product title must be unique" do
-    product = Product.new(title: products(:ruby).title,
-                          description: "An item",
-                          image_url: "somefile.jpg",
-                          price: 0.01)
+    product = new_product(0.01, products(:ruby).title)
 
     assert product.invalid?
     assert_equal ["has already been taken"],
                   product.errors[:title]
   end
 
+  test "product title must not be less than 8 characters" do
+    product = new_product(1.00, "short")
+
+    assert product.invalid?
+    assert_equal ["is too short (minimum is 8 characters)"],
+                  product.errors[:title]
+  end
+
+
+  test "product title must not be more than 100 characters" do
+    product = new_product(1.00, ('1'..'101').to_a.join)
+
+    assert product.invalid?
+    assert_equal ["is too long (maximum is 100 characters)"],
+                  product.errors[:title]
+  end
+
+  test "product description must not be less than 20 characters" do
+    product = new_product(1.00)
+    product.description = "too short"
+
+    assert product.invalid?
+    assert_equal ["is too short (minimum is 20 characters)"],
+                  product.errors[:description]
+  end
+
+  test "product description must not be more than 1000 characters" do
+    product = new_product(1.00)
+    product.description = ('1'..'1001').to_a.join
+
+    assert product.invalid?
+    assert_equal ["is too long (maximum is 1000 characters)"],
+                  product.errors[:description]
+  end
+
   test "product image_url must have proper extension" do
-    product = Product.new(title: "Title",
-                          description: "An item, really not the first",
-                          image_url: "someotherfile.image",
-                          price: 100.01)
+    product = new_product(100.01)
+    product.image_url = "someotherfile.image"
 
     assert product.invalid?
     assert_equal ["expected URL for file with an image extension"],
