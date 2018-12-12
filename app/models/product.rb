7bedfc0551700098aea5896da3d26aa4a3c5b2f3
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-11 21:00:23 -0700

    Add cart and cart fuctionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true, length: { minimum: 8, maximum: 100 }
   validates :description, length: { minimum: 20, maximum: 1000 }
@@ -10,4 +13,12 @@ class Product < ApplicationRecord
     with: /\.(jpg|jpeg|gif|png)\Z/i,
     message: 'expected URL for file with an image extension'
   }
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
