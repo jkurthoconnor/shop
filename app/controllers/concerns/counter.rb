df0694d0ce4110f5a1b4892cd63caf3a40a9e884
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-11 22:28:39 -0700

    Add cart nag counter

---
+++ app/controllers/concerns/counter.rb
@@ -0,0 +1,22 @@
+module Counter
+
+  private
+
+  def increment_counter
+    set_session_counter unless session[:counter]
+    session[:counter] += 1
+    set_ivar_counter
+  end
+
+  def reset_counter
+    set_session_counter
+  end
+
+  def set_session_counter
+    session[:counter] = 0
+  end
+
+  def set_ivar_counter
+    @counter = session[:counter]
+  end
+end
