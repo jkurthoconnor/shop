9c23fd8f2393ea3bea728a470dc6830a319178af
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-12 12:18:49 -0700

    Combine multiples into single line item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_182314) do
+ActiveRecord::Schema.define(version: 2018_12_12_184804) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_182314) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantitiy", default: 1
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
