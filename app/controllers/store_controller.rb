bf75d1617532390b7e03b635a887ce3ed58b61dc
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-16 19:06:52 -0700

    Move and restyle cart to nav

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
   include Counter
+  include CurrentCart
 
   before_action :increment_counter, only: [:index]
+  before_action :set_cart, only: [:index]
 
   def index
     @products = Product.order(:title)
