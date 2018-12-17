63790bab07f3cc6ef41df587052b99d5dc509ca5
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-16 21:25:56 -0700

    Add highlighting to new cart addition

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,7 +35,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         format.html { redirect_to store_index_url,
                       notice: 'Line item was successfully created.' }
-        format.js
+        format.js   { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
