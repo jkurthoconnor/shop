dff1c7e7f0e9c1a25e827a11dd121c03db7b3891
jkurthoconnor <jkurthoconnor@gmail.com>
2018-12-28 20:04:37 -0700

    System tests for remaining React PO Component types

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -12,10 +12,56 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in 'order_address', with: '12345 Big Court'
     fill_in 'order_email', with: 'mrguy@service.com'
 
+    assert_no_selector "#order_credit_card_number"
+    assert_no_selector "#order_expiration_date"
     assert_no_selector "#order_routing_number"
+    assert_no_selector "#order_po_number"
 
     select 'Check', from: 'pay_type'
 
     assert_selector "#order_routing_number"
+  end
+
+  test "purchase order number" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Some Guy'
+    fill_in 'order_address', with: '12345 Big Court'
+    fill_in 'order_email', with: 'mrguy@service.com'
+
+    assert_no_selector "#order_credit_card_number"
+    assert_no_selector "#order_expiration_date"
+    assert_no_selector "#order_routing_number"
+    assert_no_selector "#order_po_number"
+
+    select 'Purchase Order', from: 'pay_type'
+
+    assert_selector "#order_po_number"
+  end
+
+  test "credit card number" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Some Guy'
+    fill_in 'order_address', with: '12345 Big Court'
+    fill_in 'order_email', with: 'mrguy@service.com'
+
+    assert_no_selector "#order_credit_card_number"
+    assert_no_selector "#order_expiration_date"
+    assert_no_selector "#order_routing_number"
+    assert_no_selector "#order_po_number"
+
+    select 'Credit Card', from: 'pay_type'
+
+    assert_selector "#order_credit_card_number"
+    assert_selector "#order_expiration_date"
   end
 end
